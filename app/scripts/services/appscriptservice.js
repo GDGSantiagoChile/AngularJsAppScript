8dbf0433540734f6b74b1c56da19c8b4cb42d89e
Christian Rojas <christiandiegor@gmail.com>
2015-04-04 15:41:35 -0300

    se crea factory de listado

--- app/scripts/services/appscriptservice.js
+++ app/scripts/services/appscriptservice.js
@@ -8,12 +8,15 @@
  * Service in the angularclApp.
  */
 angular.module( 'angularclApp' )
+
   .service( 'appScriptService', function ( $resource ) {
 
   	var url = 'https://script.google.com/macros/s/AKfycbxt4LCprQGTu00NkWDwUBHhIy4GCosUj3NjV5sG8su46ej4R8k/exec';
 
   	return $resource( url, {}, {
+
   		'put': { method: 'post', isArray: false, url: url + ':values' },
+  		
   	});
 
   });
